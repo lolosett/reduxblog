47fbd972fffa75a18457b1c3cc175dff71ced50c
Lauren Settle <laurjsett@gmail.com>
2016-09-25 16:17:03 -0700

    [pull] creates reducer for fetching posts

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,8 +4,10 @@ export const FETCH_POSTS = 'FETCH_POSTS';
 const ROOT_URL='http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=123'
 
-export function fetchPosts(){
-  const request = axios.get({`${ROOT_URL}/posts${API_KEY}`})
+
+export function fetchPosts() {
+
+  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
   return {
     type: FETCH_POSTS,
