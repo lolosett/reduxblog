7afb37fd16619a08bede967a2c3f052eb327ee0f
Lauren Settle <laurjsett@gmail.com>
2016-09-25 15:51:13 -0700

    [pull] adds posts index component and routing

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 import App from './components/app';
+import PostsIndex from './components/posts_index';
 
-const Greeting = () => {
-  return <div>Hey There</div>
-};
 
 export default (
   <Route path="/" component={App}>
-    <Route path="greet" component={Greeting} />
-    <Route path="greet2" component={Greeting} />
-    <Route path="greet3" component={Greeting} />
+    <IndexRoute component={PostsIndex} />
+
   </Route>
 
 );
