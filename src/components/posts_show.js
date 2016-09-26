b413d85d0e503996e059cc1e94797bf1b3843c6d
Lauren Settle <laurjsett@gmail.com>
2016-09-25 23:28:48 -0700

    [feature] completes show posts feature

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,13 +1,25 @@
-import React, { Component } from 'react';
-import { fetchPost } from '../actions/index';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import { fetchPost, deletePost } from '../actions/index';
 
 class PostsShow extends Component {
+
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
   componentWillMount() {
     this.props.fetchPost(this.props.params.id);
   }
+
+  onDeleteClick() {
+     this.props.deletePost(this.props.params.id)
+     .then(() => {
+       this.context.router.push('/');
+     });
+   }
+
   render(){
     const {post} = this.props;
     if(!post) {
@@ -16,6 +28,7 @@ class PostsShow extends Component {
     return(
       <div>
         <Link to='/'>Back to Index</Link>
+        <button className="btn btn-danger pull-xs-right" onClick={this.onDeleteClick.bind(this)}>Delete Post</button>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
